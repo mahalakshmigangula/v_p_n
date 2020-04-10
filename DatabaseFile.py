586005a5af85055812f741e4a69e8675c5ae2b5d
Prashant Choudhari <60341906+prash-10520536@users.noreply.github.com>
2020-04-10 21:24:44 +0100

    database queries

--- DatabaseFile.py
+++ DatabaseFile.py
@@ -7,16 +7,40 @@ import sqlite3
 
 class DatabaseFile:
     def __init__(self):
-        self.dbConnection = sqlite3.connect("regDbFile.db")
+        self.dbConnection = sqlite3.connect("regDBFile.db") #database file will create using name "regDBFile.db"
         self.dbCursor = self.dbConnection.cursor()
-        self.dbCursor.execute(
-            "CREATE TABLE IF NOT EXISTS user_record (id integer PRIMARY KEY, fName text, lName text, dob text, mob text, yob text, gender text, address text, phoneNum text, emailId text, indoorGame text, OutdoorGame text)")
+        self.dbCursor.execute("CREATE TABLE IF NOT EXISTS user_record (id integer PRIMARY KEY, fName text, lName text, dob text, mob text, yob text, gender text, address text, phoneNum text, emailId text, indoorGame text, OutdoorGame text)")
 
     def __del__(self):
         self.dbCursor.close()
-        self.dbConnection.close()
+        self.dbConnection.close() #to close DB connection
 
-        def Insert(self, id, fName, lName, dob, mob, yob, gender, address, phoneNum, emailId, indoorGame, outdoorGame):
-            self.dbCursor.execute("INSERT INTO patient_info VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (
+#inserting data into database
+    def Insert(self, id, fName, lName, dob, mob, yob, gender, address, phoneNum, emailId, indoorGame, outdoorGame):
+            self.dbCursor.execute("INSERT INTO user_record VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (
                 id, fName, lName, dob, mob, yob, gender, address, phoneNum, emailId, indoorGame, outdoorGame))
             self.dbConnection.commit()
+
+#to update data in the database
+    def Update(self, id, fName, lName, dob, mob, yob, gender, address, phoneNum, emailId, indoorGame, outdoorGame):
+        self.dbCursor.execute(
+            "UPDATE user_record SET fName = ?, lName = ?, dob = ?, mob = ?, yob = ?, gender = ?, address = ?, phoneNum = ?, emailId = ?, indoorGame = ?, outdoorGame = ? WHERE id = ?",
+            (id, fName, lName, dob, mob, yob, gender, address, phoneNum, emailId, indoorGame, outdoorGame))
+        self.dbConnection.commit()
+
+#to search specific user details based on ID
+    def Search(self, id):
+        self.dbCursor.execute("SELECT * FROM user_record WHERE id = ?", (id,))
+        searchResults = self.dbCursor.fetchall()
+        return searchResults
+
+#to delete specific user details based on ID
+    def Delete(self, id):
+        self.dbCursor.execute("DELETE FROM user_record WHERE id = ?", (id,))
+        self.dbConnection.commit()
+
+#to display specific user details based on ID
+    def Display(self):
+        self.dbCursor.execute("SELECT * FROM user_record")
+        records = self.dbCursor.fetchall()
+        return records
